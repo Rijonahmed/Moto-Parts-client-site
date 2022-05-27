772162d1cf7a361b147323f02747680bef818045
Rijonahmed <rijon036@gmail.com>
2022-05-27 11:27:02 +0600

    bolg and my protfolio added

--- src/Pages/Text-Page/Blog.js
+++ src/Pages/Text-Page/Blog.js
@@ -2,10 +2,40 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <div>
-      <h1>question ans</h1>
+    <div className='mx-12'>
+
+      <div className='my-5'>
+        <h1 className='text-2xl my-3'><big>Question: </big>How will you improve the performance of a React Application?</h1>
+        <p><big className='font-bold'>Ans: </big>To optimize React rendering, you need to make sure that components receive only necessary props. It will let you control the CPU consumption and avoid over-rendering unnecessary features. The solution is to create a functional component that will collect all props and redistribute them to other components.</p>
+
+      </div>
+
+      <div className='my-5'>
+        <h1 className='text-2xl my-3'><big>Question: </big>What are the different ways to manage a state in a React application?</h1>
+        <p><big className='font-bold'>Ans: </big>The state is a built-in React object that is used to contain data or information about the component.useState() useState() hook allows you create and mange a state variable that can be a simple JavaScript primitive or an object.In a React component, props are variables passed to it by its parent component. State on the other hand is still variables, but directly initialized and managed by the component.</p>
+
+      </div>
+
+      <div className='my-5'>
+        <h1 className='text-2xl my-3'><big>Question: </big>How does prototypical inheritance work?</h1>
+        <p><big className='font-bold'>Ans: </big>Prototype-based programming is a style of object-oriented programming in which behaviour reuse (known as inheritance) is performed via a process of reusing existing objects that serve as prototypes. Classical inheritance is limited to classes inheriting from other classes. However prototypal inheritance includes not only prototypes inheriting from other prototypes but also objects inheriting from prototypes.</p>
+
+      </div>
+      <div className='my-5'>
+        <h1 className='text-2xl my-3'><big>Question: </big>Why you do not set the state directly in React ? For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts</h1>
+        <p><big className='font-bold'>Ans: </big>If the setter of state is unused, you can avoid it the useState . You can keep the value in a const (in the body of the component), or move outside of the component. setState is used when it's needed to be aware of the changes in the value stored in state. This way the component knows when to re-render.</p>
+
+      </div>
+
+
+      <div className='my-5'>
+        <h1 className='text-2xl my-3'><big>Question: </big>What is a unit test? Why should write unit tests?</h1>
+        <p><big className='font-bold'>Ans: </big>Unit testing is testing the smallest testable unit of an application. It is done during the coding phase by the developers. To perform unit testing, a developer writes a piece of code (unit tests) to verify the code to be tested (unit) is correct.</p>
+
+      </div>
 
     </div>
+
   );
 };
 
--- src/Pages/Text-Page/MyPortfolio.js
+++ src/Pages/Text-Page/MyPortfolio.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import myImages from '../../image/rijon.jpg'
 
 const MyPortfolio = () => {
   return (
     <div>
-      <h1>My Portfolio</h1>
 
+      <div class="card w-96 m-auto bg-base-100 shadow-xl mt-14">
+        <figure class="px-10 pt-10">
+          <img className='rounded-full w-2/4 items-center' src={myImages} alt="" />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h1 className='text-2xl font-bold'>Rijon Ahned</h1>
+          <h4 className='text-xl'>Email Address : rijon036@gmail.com</h4>
+
+        </div>
+      </div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 mx-12'>
+
+
+        <div class="card bg-base-100 shadow-xl my-20">
+          <span className='card-body'>
+            <h1 className='text-xl m-8 font-bold'>List of technologies or skills that to me as a web developer.</h1>
+
+
+            <p>&#8226; I can make react application.</p>
+            <p>&#8226; I can implement firebase authentication system.</p>
+            <p>&#8226; I can use node js for backend.</p>
+            <p>&#8226; I can use mongodb for database.</p>
+            <p>&#8226; I can reduce the quantity of any product.</p>
+            <p>&#8226; I can do CRUD (Create, Read, Update, Delete) operations.</p>
+            <h2 className='text-xl font-bold'>My project Link</h2>
+            <p className='text-success font-bold'><a href="https://moto-dealer.web.app/" target="_blank" rel="noopener noreferrer"> Project one</a></p>
+            <p className='text-success font-bold'><a href="https://best-wedding-photographer.web.app/" target="_blank" rel="noopener noreferrer"> Project two</a></p>
+            <p className='text-success font-bold'><a href="https://favourite-bike-choose-for-1.netlify.app/" target="_blank" rel="noopener noreferrer"> Project three</a></p>
+
+          </span>
+
+        </div>
+
+
+        <div class="card bg-base-100 shadow-xl my-20">
+          <div class="card-body">
+            <h1 className='text-2xl font-bold'>Educational background</h1>
+            <h4 className='text-xl'>&#8226; SSC batch-2017 from Amirjan High School in Dumni, Khilkhet, Dhaka</h4>
+            <h4 className='text-xl'>&#8226; HSC batch-2019 from Amirjan College in Dumni, Khilkhet, Dhaka</h4>
+            <h4 className='text-xl'>&#8226; Now i am studing in Honor's 2nd year from Uttara Town Colege in Uttara,Abdullapur, Dhaka</h4>
+
+          </div>
+        </div>
+
+
+      </div>
     </div>
+
   );
 };
 
